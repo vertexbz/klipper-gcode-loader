ef9e5215d4f7211fcd1cd5118dfbf9c7236ff5e4
vertex <adam.makswiej@gmail.com>
2023-11-10 03:16:45 +0100

    tweak error message

--- src/line/error.py
+++ src/line/error.py
@@ -6,15 +6,16 @@ from gcode import CommandError
 class LineError(Exception):
     def __init__(self, line: GCodeLine, *args):
         if len(args) == 1 and isinstance(args[0], BaseException):
-            args = (str(args[0]), args[0])
-        super().__init__(*args)
+            super().__init__(str(args[0]))
+        else:
+            super().__init__(*args)
         self.line = line
 
 
 class CommandLineError(CommandError):
     def __init__(self, line: GCodeLine, *args):
         if len(args) == 1 and isinstance(args[0], BaseException):
-            args = (str(args[0]), args[0])
-        super().__init__(*args)
+            super().__init__(str(args[0]))
+        else:
+            super().__init__(*args)
         self.line = line
-
